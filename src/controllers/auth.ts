0ad791491e6905f27b41f5d45e8f21e187367e16
Michael <damilola.babalola@decagon.dev>
2021-08-26 23:45:28 +0100

    routers

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-var-requires */
-import { NextFunction, Response } from 'express';
-import jwt from 'jsonwebtoken';
+import { NextFunction, Response } from "express";
+import jwt from "jsonwebtoken";
 // import config from 'config'
-import User from '../models/usersModel';
+import User from "../models/usersModel";
 const secret: string = process.env.JWT_SECRET as string;
 export async function auth(
   req: any,
@@ -16,9 +16,9 @@ export async function auth(
     const token = req.cookies.jwt;
     const decoded: any = jwt.verify(token, secret);
     console.log(decoded);
-    const user = await User.findOne({ _id: decoded.id, 'tokens.token': token });
+    const user = await User.findOne({ _id: decoded.id, "tokens.token": token });
     if (!user) {
-      throw new Error('Thrown here');
+      throw new Error("Thrown here");
     }
     req.token = token;
     req.user = user;
@@ -26,6 +26,54 @@ export async function auth(
   } catch (e) {
     console.log(e);
     // res.status(401).send({ error: 'Please authenticate.' })
-    res.redirect('/holidayresort');
+    res.redirect("/holidayresort");
+  }
+}
+
+export async function auth1(
+  req: any,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  try {
+    console.log(req.cookies);
+    const token = req.cookies.jwt;
+    const decoded: any = jwt.verify(token, secret);
+    console.log(decoded);
+    const user = await User.findOne({ _id: decoded.id, "tokens.token": token });
+    if (!user) {
+      throw new Error("Thrown here");
+    }
+    req.token = token;
+    req.user = user;
+    next();
+  } catch (e) {
+    console.log(e);
+    // res.status(401).send({ error: 'Please authenticate.' })
+    res.redirect("/login");
+  }
+}
+
+export async function auth3(
+  req: any,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  try {
+    console.log(req.cookies);
+    const token = req.cookies.jwt;
+    const decoded: any = jwt.verify(token, secret);
+    console.log(decoded);
+    const user = await User.findOne({ _id: decoded.id, "tokens.token": token });
+    if (!user) {
+      throw new Error("Thrown here");
+    }
+    req.token = token;
+    req.user = user;
+    next();
+  } catch (e) {
+    console.log(e);
+    // res.status(401).send({ error: 'Please authenticate.' })
+    res.redirect("/login");
   }
 }
--- src/routes/memberRouter.ts
+++ src/routes/memberRouter.ts
@@ -1,9 +1,9 @@
-import { Router } from 'express';
-import { auth } from '../controllers/auth';
+import { Router } from "express";
+import { auth1 } from "../controllers/auth";
 const router = Router();
 
-router.get('/', auth, (req, res) => {
-  res.render('membership');
+router.get("/", auth1, (req, res) => {
+  res.render("membership");
 });
 
 export default router;
